8138bd523c27fb6310bb28ecc5a8b38f5110f979
1q2zb <127278700+1q2zb@users.noreply.github.com>
2023-07-21 14:09:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,33 @@
 
 using namespace std;
 
-string pass,language,cinrule,temp;
-int f1,f2;
+string passin,passout,language,cinrule,temp;
+long long f1,f2,num;
 string lang[2];
 string res;
+string tempstring;
 
-int get(string a){//判断注释以保证注释后文字不会被翻译
-    int t=0;
-    for(int i=0;i<a.length();i++){
+void strnum(){//字符串转换为数字
+    num=0;
+    for(long long i=0;i<tempstring.length();i++){
+        num=num*10+tempstring[i]-'0';
+    }
+    return;
+}
+
+long long get(string a){//判断注释以保证注释后文字不会被翻译
+    long long t=0;
+    for(long long i=0;i<a.length();i++){
         if(t==1&&a[i]!='/')t=0;
         if(a[i]=='/')t++;
         if(t==2)return i-1;//返回注释前第一个'/'的位置
     }
-    return a.length();
+    return a.length();//返回行长度+1
 }
 
-void str_find(string a){
+void str_find(string a){//找到输入和输出语言所在位置并存储
     a.insert(0," ");//插入空格以保证f1和f2取值统一
-    int x1=-1,x2=-1;int t=0;int i=0;//x1x2标记位置
+    long long x1=-1,x2=-1;long long t=0;long long i=0;//x1x2标记位置
     lang[0]=lang[1]="";//定义输出
     for(;i<a.length();i++){
         if(a[i]==' ')t++;//换语言
@@ -37,32 +46,50 @@ int main()
 {
     fstream cin1,cin2,cout1;//打开文件
     cin1.open("language.txt",fstream::in);//打开设定文件
-    cout1.open("result",fstream::out);//输出文件
+    getline(cin1,tempstring);//获取语言数量以便集中操作
+    strnum();
     getline(cin1,language);//获取输出规则
-    getline(cin1,cinrule);//获取输出规则
-    cout<<cinrule;
-    cin>>pass;//填写文件路径
-    cin2.open(pass.c_str(),fstream::in);//bzd怎么写
-    getline(cin1,cinrule);//获取输出规则
-    cout<<language<<endl<<cinrule;
-    cin>>f1;//输入语言
-    getline(cin1,cinrule);//获取输出规则
-    cout<<cinrule;
-    cin>>f2;//输出语言
+    for(long long i=0;i<num;i++){//引导填写原始文件路径
+        getline(cin1,cinrule);
+        cout<<cinrule<<endl;
+    }
+    cin>>passin;//填写原始文件路径
+    cin2.open(passin.c_str(),fstream::in);//打开原始文件
+    for(long long i=0;i<num;i++){//引导填写更改后文件路径
+        getline(cin1,cinrule);
+        cout<<cinrule<<endl;
+    }
+    cin>>passout;//填写更改后文件路径
+    cout1.open(passout.c_str(),fstream::out);//打开输出文件
+    cout<<language<<endl;//引导输入语言
+    for(long long i=0;i<num;i++){//引导输入原始语言
+        getline(cin1,cinrule);
+        cout<<cinrule<<endl;
+    }
+    cin>>f1;//输入原始语言
+    for(long long i=0;i<num;i++){//引导输入更改后语言
+        getline(cin1,cinrule);
+        cout<<cinrule<<endl;
+    }
+    cin>>f2;//输入更改后语言
     while(getline(cin2,res)){//读取每一行程序
         cin1.clear();//清除语言文件读取进度
         cin1.seekg(0,ios::beg);
-        for(int i=0;i<4;i++)getline(cin1,temp);
+        for(long long i=0;i<4*num+2;i++)getline(cin1,temp);//读取进度快进到主体
         while(getline(cin1,language)){//每一条语言
-            str_find(language);//找到输入和输出语言所在位置并存储
-            int index;
-            while(res.find(lang[0])>=0&&res.find(lang[0])<get(res)){//判断是否存在
+            str_find(language);//找到原始和更改后语言位置
+//            cout<<lang[0]<<" "<<lang[1]<<endl;
+            long long index;//标记位置
+            while(res.find(lang[0])>=0&&res.find(lang[0])<get(res)){//判断注释前是否存在关键词
                 index=res.find(lang[0]);//获取原语言所在位置
                 res.erase(index,lang[0].length());//删除原语言
                 res.insert(index,lang[1]);//改为新语言
             }
         }
-        cout1<<res<<endl;
+        cout1<<res<<endl;//输出到更改后文件
     }
     return 0;
 }
+//这就是完整的翻译程序了。
+//
+//              ——by 1q2zb
